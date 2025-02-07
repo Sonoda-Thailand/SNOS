7ffec667d394483d822e9cf098830c9e0eee0753
PeterSam <panupophahaha@gmail.com>
2025-02-07 15:39:46 +0700

    Add Year

--- SNOS_Report/Services/ErrorService.cs
+++ SNOS_Report/Services/ErrorService.cs
@@ -2,6 +2,7 @@
 using SNOS_Report.Models;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 
@@ -28,6 +29,26 @@ namespace SNOS_Report.Services
                 This_Count = x.Count(t => t.month == month && t.year == year) != 0 ? x.FirstOrDefault(t => t.month == month && t.year == year).Count : 0,
                 Last_Count = x.Count(t => t.month != month) != 0 ? x.FirstOrDefault(t => t.month != month).Count : 0
 
+            }).ToList();
+
+            return result;
+        }
+
+        public List<Error_Compair> GetErrorMonthlyYearly(int line, string lang, int year)
+        {
+            var totalData = new List<Error_Total>();
+            var lastYear_Data = db.GetErrorByYear(line, lang, year - 1);
+            var thisYear_Data = db.GetErrorByYear(line, lang, year);
+            totalData.AddRange(lastYear_Data);
+            totalData.AddRange(thisYear_Data);
+
+            var result = totalData.GroupBy(x => x.Error_No).Select(x => new Error_Compair
+            {
+                Error_No = x.Key,
+                Line = x.FirstOrDefault().Line,
+                Title = x.FirstOrDefault().Title,
+                This_Count = x.Count(t => t.year == year && t.year == year) != 0 ? x.FirstOrDefault(t => t.year == year && t.year == year).Count : 0,
+                Last_Count = x.Count(t => t.year != year) != 0 ? x.FirstOrDefault(t => t.year != year).Count : 0
             }).ToList();
 
             return result;
